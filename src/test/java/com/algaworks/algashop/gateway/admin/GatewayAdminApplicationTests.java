74fe906d5a0d0b47e0dad198bf11c545b27ede3d
Jean Malvessi <jeanmalvessi@gmail.com>
2026-09-06 16:09:56 -0300

    Gateway Admin initial commit

---
+++ src/test/java/com/algaworks/algashop/gateway/admin/GatewayAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algashop.gateway.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
